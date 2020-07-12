460df2f8c55e7847657736ef34c0ea556476c418
myk245 <myk245@nyu.edu>
2020-07-12 00:09:26 -0400

    added images and logo

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Toolbar, IconButton, Typography} from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Typography, Avatar} from '@material-ui/core';
 import MenuRoundedIcon from '@material-ui/icons/MenuRounded';
 
 export default function NavBar() {
@@ -7,11 +7,16 @@ export default function NavBar() {
       <div>
          <AppBar position="static" style={{ background: 'white' }}>
             <Toolbar>
+               <Avatar
+                  alt="Cindy Kei logo"
+                  src="cindy-logo.png"
+                  class="avatar-logo"
+               />
                <IconButton edge="start" aria-label="menu">
                   <MenuRoundedIcon />
                </IconButton>
-               <Typography variant="h6">
-                  News
+               <Typography variant="h6" class="navbar">
+                  Options
                </Typography>
             </Toolbar>
          </AppBar>
