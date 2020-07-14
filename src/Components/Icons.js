a0e11028db202f5277a5d34055422d5ce35b6598
myk245 <myk245@nyu.edu>
2020-07-13 21:59:25 -0400

    fixed up styling for icons bar

--- src/Components/Icons.js
+++ src/Components/Icons.js
@@ -1,11 +1,19 @@
 import React from 'react'; 
 import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import EmailSharpIcon from '@material-ui/icons/EmailSharp';
 
 function Icons() {
    return (
       <div>
-         <a href="https://github.com/myk245" target="_blank">
-         <GitHubIcon fontSize="large" />
+         <a href="https://www.linkedin.com/in/cindy-kei/" target="_blank" class="icons-bar">
+            <LinkedInIcon fontSize="large" />
+         </a>
+         <a href="https://github.com/myk245" target="_blank" class="icons-bar">
+            <GitHubIcon fontSize="large" />
+         </a>
+         <a href="mailto:cindykei92@gmail.com" target="_blank" class="icons-bar">
+            <EmailSharpIcon fontSize="large" color="black"/>
          </a>
       </div>
    )
