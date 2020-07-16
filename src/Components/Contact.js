04b2fa1c1747e660035eb00e5e5997f346caf0f6
myk245 <myk245@nyu.edu>
2020-07-15 22:25:00 -0400

    added contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,15 +1,75 @@
 import React from 'react';
+import { TextField, Paper, Button, Icon } from '@material-ui/core';
 
-function Contact() {
-   return (
-      <div>
-         <br></br>
-         <br></br>
-         Contact Form Coming Soon!
-         <br></br>
-         <br></br>
-      </div>
-   )
+class Contact extends React.Component {
+
+   state = {
+      name: "", 
+      email: "", 
+      message: ""
+   }
+
+   handleChange = (event) => {
+      this.setState({
+         name: event.target.value
+      })
+   }
+
+   render() {  
+      return (
+         <div>
+            <br></br>
+            <br></br>
+            <br></br>
+            <Paper id="contact-form" elevation={3} variant="outlined">
+               <form>
+                  <TextField
+                     label="Name"
+                     type="text"
+                     name="name"
+                     multiline
+                     rows={1}
+                     variant="outlined"
+                     size="small"
+                     id="name-field"
+                     onChange={this.handleChange}
+                  />
+                  <br></br>
+                  <TextField
+                     label="Email"
+                     type="email"
+                     name="email"
+                     multiline
+                     rows={1}
+                     variant="outlined"
+                     size="small"
+                     id="email-field"
+                     onChange={this.handleChange}
+                  />
+                  <br></br>
+                  <TextField
+                     label="Message"
+                     name="message"
+                     multiline
+                     rows={5}
+                     variant="outlined"
+                     size="small"
+                     id="message-field"
+                     onChange={this.handleChange}
+                  />
+
+                  <br></br>
+                  <br></br>
+                  <Button variant="outlined" color="primary" size="small" endIcon={<Icon>send</Icon>}>Send Message</Button>
+ 
+               </form>
+            </Paper>
+            <br></br>
+            <br></br>
+            <br></br>
+         </div>
+      )
+   }
 }
 
 export default Contact; 
