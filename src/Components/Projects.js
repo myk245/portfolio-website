7f2d6bdc825a051ee50d9d2dbdef3241c60062c7
myk245 <myk245@nyu.edu>
2020-07-20 18:42:32 -0400

    project cards displayed in a row

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -18,7 +18,7 @@ function Projects() {
                spacing={2}
                id="grid-container"
             >
-               <Grid item xs={10}>
+               <Grid item xs={4}>
                   <Card class="project-card">
                         <Typography gutterBottom variant="h5" component="h2">
                            Virago
@@ -38,8 +38,8 @@ function Projects() {
                      <Typography>More Details→ </Typography>
                   </Card>
                </Grid>
-               
-               <Grid item xs={10}>
+
+               <Grid item xs={4}>
                   <Card class="project-card">
                      <Typography gutterBottom variant="h5" component="h2">
                         Virago
@@ -59,6 +59,28 @@ function Projects() {
                      <Typography>More Details→ </Typography>
                   </Card>
                </Grid>
+
+               <Grid item xs={4}>
+                  <Card class="project-card">
+                     <Typography gutterBottom variant="h5" component="h2">
+                        Virago
+                     </Typography>
+                     <CardMedia
+                        component="img"
+                        alt="A Mental Health Care App"
+                        height="140"
+                        image="https://www.nomadfoods.com/wp-content/uploads/2018/08/placeholder-1-e1533569576673.png"
+                        title="Virago"
+                     />
+                     <CardContent>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                           A centralized resource and community support group for mental health care.
+                        </Typography>
+                     </CardContent>
+                     <Typography>More Details→ </Typography>
+                  </Card>
+               </Grid>
+               
             </Grid>
          </Paper>
          <br></br>
