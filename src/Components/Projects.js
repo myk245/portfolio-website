8301e9b61140457ea6e1014f7ce3be8e8bd859fe
myk245 <myk245@nyu.edu>
2020-07-19 23:08:42 -0400

    began rendering cards for project details

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Paper } from '@material-ui/core';
+import { Grid, Paper, Card, CardContent, CardMedia, Typography} from '@material-ui/core';
 
 function Projects() {
    return (
@@ -11,6 +11,48 @@ function Projects() {
                <h1 class="page-title">My Projects</h1>
                <h6 class="page-subtitle">Click on a project below to view further details.</h6>
             </div>
+            <Grid container spacing={20}>
+               <Grid item md={10}>
+                  <Card class="project-card">
+                        <Typography gutterBottom variant="h5" component="h2">
+                           Virago
+                        </Typography>
+                        <CardMedia
+                           component="img"
+                           alt="A Mental Health Care App"
+                           height="140"
+                           image="https://www.nomadfoods.com/wp-content/uploads/2018/08/placeholder-1-e1533569576673.png"
+                           title="Virago"
+                        />
+                        <CardContent>
+                           <Typography variant="body2" color="textSecondary" component="p">
+                              A centralized resource and community support group for mental health care.
+                           </Typography>
+                     </CardContent>
+                     <Typography>More Details→ </Typography>
+                  </Card>
+               </Grid>
+               <Grid item md={10}>
+                  <Card class="project-card">
+                     <Typography gutterBottom variant="h5" component="h2">
+                        Virago
+                     </Typography>
+                     <CardMedia
+                        component="img"
+                        alt="A Mental Health Care App"
+                        height="140"
+                        image="https://www.nomadfoods.com/wp-content/uploads/2018/08/placeholder-1-e1533569576673.png"
+                        title="Virago"
+                     />
+                     <CardContent>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                           A centralized resource and community support group for mental health care.
+                        </Typography>
+                     </CardContent>
+                     <Typography>More Details→ </Typography>
+                  </Card>
+               </Grid>
+            </Grid>
          </Paper>
          <br></br>
          <br></br>
