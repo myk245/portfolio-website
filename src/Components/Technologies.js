905be61dd6db8e9a5325c40fa8dfaa24deb85f01
myk245 <myk245@nyu.edu>
2020-07-20 23:11:00 -0400

    added fontawesome and devicon icons to technologies

--- src/Components/Technologies.js
+++ src/Components/Technologies.js
@@ -1,29 +1,38 @@
 import React from 'react';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import EmailSharpIcon from '@material-ui/icons/EmailSharp';
-import TwitterIcon from '@material-ui/icons/Twitter';
-import { Divider } from '@material-ui/core';
-
+import { Paper, Divider } from '@material-ui/core';
+ 
 function Icons() {
    return (
       <div>
-         <Divider />
          <br></br>
-         <a href="https://www.linkedin.com/in/cindy-kei/" target="_blank" class="icons-bar">
-            <LinkedInIcon fontSize="large" />
-         </a>
-         <a href="https://github.com/myk245" target="_blank" class="icons-bar">
-            <GitHubIcon fontSize="large" />
-         </a>
-         <a href="https://twitter.com/KeiCindy" target="_blank" class="icons-bar">
-            <TwitterIcon fontSize="large" color="black" />
-         </a>
-         <a href="mailto:cindykei92@gmail.com" target="_blank" class="icons-bar">
-            <EmailSharpIcon fontSize="large" color="black" />
-         </a>
          <br></br>
-         <p id="connect-with">Let's connect!</p>
+         <Paper class="paper-section" elevation={3} variant="outlined">
+            <div id="tech-div">
+               <h1 class="page-title">Technologies</h1>
+               <span class="page-subtitle">Below are the technologies that I am experienced in.</span>
+               <br></br>
+               <br></br>
+               <Divider />
+               <br></br>
+               <br></br>
+               <span class="fab fa-js-square fa-10x"></span>
+               <p class="tech-p">JavaScript</p>
+               <br></br>
+               <span class="fab fa-react fa-10x fa-spin"></span>
+               <p class="tech-p">React</p>
+               <br></br>
+               <span class="devicon-ruby-plain"></span>
+               <p class="tech-p">Ruby</p>
+               <br></br>
+               <span class="devicon-rails-plain"></span>
+               <p class="tech-p">Rails</p>
+               <br></br>
+               <br></br>
+               <h3>More To Come!</h3>
+            </div>
+         </Paper>  
+         <br></br>
+         <br></br>
       </div>
    )
 }
