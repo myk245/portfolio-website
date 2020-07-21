b31007daa0a312d4c07b183fe2b4639136505096
myk245 <myk245@nyu.edu>
2020-07-20 20:49:14 -0400

    centered items in flex box

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -6,20 +6,43 @@ function Projects() {
       <div>
          <br></br>
          <br></br>
-         <div>
             <h1 class="page-title">My Projects</h1>
             <h6 class="page-subtitle">Click on a project below to view further details.</h6>
-         </div>
          <Paper class="paper-section" elevation={3} variant="outlined">
             <br></br>
             <br></br>
-            <Grid
-               container
-               spacing={2}
-               id="grid-container"
-            >
-               <Grid item xs={4}>
-                  <Card class="project-card">
+            <div id="grid-div">
+               <Grid container spacing={6} id="grid-container">
+                  
+                  <Grid item
+                     xs={12} sm={6} md={4}
+                     className="grid-card">
+                     <Card class="project-card">
+                           <Typography gutterBottom variant="h5" component="h2">
+                              Virago
+                           </Typography>
+                           <CardMedia
+                              component="img"
+                              alt="A Mental Health Care App"
+                              height="140"
+                              image="https://www.nomadfoods.com/wp-content/uploads/2018/08/placeholder-1-e1533569576673.png"
+                              title="Virago"
+                           />
+                           <CardContent>
+                              <Typography variant="body2" color="textSecondary" component="p">
+                                 A centralized resource and community support group for mental health care.
+                              </Typography>
+                        </CardContent>
+                        <Typography>More Details→ </Typography>
+                     </Card>
+                  </Grid>
+
+                  <Grid
+                     item
+                     xs={12} sm={6} md={4}
+                     className="grid-card"
+                  >
+                     <Card class="project-card">
                         <Typography gutterBottom variant="h5" component="h2">
                            Virago
                         </Typography>
@@ -34,54 +57,39 @@ function Projects() {
                            <Typography variant="body2" color="textSecondary" component="p">
                               A centralized resource and community support group for mental health care.
                            </Typography>
-                     </CardContent>
-                     <Typography>More Details→ </Typography>
-                  </Card>
-               </Grid>
+                        </CardContent>
+                        <Typography>More Details→ </Typography>
+                     </Card>
+                  </Grid>
 
-               <Grid item xs={4}>
-                  <Card class="project-card">
-                     <Typography gutterBottom variant="h5" component="h2">
-                        Virago
-                     </Typography>
-                     <CardMedia
-                        component="img"
-                        alt="A Mental Health Care App"
-                        height="140"
-                        image="https://www.nomadfoods.com/wp-content/uploads/2018/08/placeholder-1-e1533569576673.png"
-                        title="Virago"
-                     />
-                     <CardContent>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                           A centralized resource and community support group for mental health care.
+                  <Grid
+                     item
+                     xs={12} sm={6} md={4}
+                     className="grid-card"
+                  >
+                     <Card class="project-card">
+                        <Typography gutterBottom variant="h5" component="h2">
+                           Virago
                         </Typography>
-                     </CardContent>
-                     <Typography>More Details→ </Typography>
-                  </Card>
-               </Grid>
-
-               <Grid item xs={4}>
-                  <Card class="project-card">
-                     <Typography gutterBottom variant="h5" component="h2">
-                        Virago
-                     </Typography>
-                     <CardMedia
-                        component="img"
-                        alt="A Mental Health Care App"
-                        height="140"
-                        image="https://www.nomadfoods.com/wp-content/uploads/2018/08/placeholder-1-e1533569576673.png"
-                        title="Virago"
-                     />
-                     <CardContent>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                           A centralized resource and community support group for mental health care.
-                        </Typography>
-                     </CardContent>
-                     <Typography>More Details→ </Typography>
-                  </Card>
-               </Grid>
-               
-            </Grid>
+                        <CardMedia
+                           component="img"
+                           alt="A Mental Health Care App"
+                           height="140"
+                           image="https://www.nomadfoods.com/wp-content/uploads/2018/08/placeholder-1-e1533569576673.png"
+                           title="Virago"
+                        />
+                        <CardContent>
+                           <Typography variant="body2" color="textSecondary" component="p">
+                              A centralized resource and community support group for mental health care.
+                           </Typography>
+                        </CardContent>
+                        <Typography>More Details→ </Typography>
+                     </Card>
+                  </Grid>
+                  </Grid>
+            </div>
+            <br></br>
+            <br></br>
          </Paper>
          <br></br>
          <br></br>
