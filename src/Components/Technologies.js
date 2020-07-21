24e30db08eca3e492d1a5d144ad0e46b18cd8e18
myk245 <myk245@nyu.edu>
2020-07-20 21:10:29 -0400

    added technologies page

---
+++ src/Components/Technologies.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import EmailSharpIcon from '@material-ui/icons/EmailSharp';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import { Divider } from '@material-ui/core';
+
+function Icons() {
+   return (
+      <div>
+         <Divider />
+         <br></br>
+         <a href="https://www.linkedin.com/in/cindy-kei/" target="_blank" class="icons-bar">
+            <LinkedInIcon fontSize="large" />
+         </a>
+         <a href="https://github.com/myk245" target="_blank" class="icons-bar">
+            <GitHubIcon fontSize="large" />
+         </a>
+         <a href="https://twitter.com/KeiCindy" target="_blank" class="icons-bar">
+            <TwitterIcon fontSize="large" color="black" />
+         </a>
+         <a href="mailto:cindykei92@gmail.com" target="_blank" class="icons-bar">
+            <EmailSharpIcon fontSize="large" color="black" />
+         </a>
+         <br></br>
+         <p id="connect-with">Let's connect!</p>
+      </div>
+   )
+}
+
+export default Icons; 
