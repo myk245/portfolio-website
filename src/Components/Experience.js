dbed51c890cffffe76d2a70bc8e9277e243f352f
myk245 <myk245@nyu.edu>
2020-08-19 15:21:32 -0400

    begin to rework experience page

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -1,5 +1,5 @@
 import React from 'react'; 
-import { Paper } from '@material-ui/core'; 
+import { Paper, Card } from '@material-ui/core'; 
 
 function Experience() {
    return (
@@ -8,11 +8,17 @@ function Experience() {
          <br></br>
          <h1 class="page-title">Experience</h1>
          <Paper class="paper-section" elevation={3} variant="outlined">
-            <img
+            <Card class="experience-card">
+               <div>
+                  <p class="company-name">Simon & Schuster</p>
+                  <p>Subsidiary Rights Associate</p>
+               </div>
+            </Card>
+            {/* <img
                src="experience.png"
                alt="resume"
                id="resume"
-            />
+            /> */}
          </Paper>
          <br></br>
          <br></br>
