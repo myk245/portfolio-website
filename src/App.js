f55f5fd40bb8afdd7e4591bfac490113c3f5aa2e
myk245 <myk245@nyu.edu>
2020-08-27 15:36:07 -0400

    solves issue with dependencies involving jest

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import Logo from './Components/Logo'; 
+import Logo from './Components/Logo';
 import NavBar from './Components/NavBar';
-import Icons from './Components/Icons'; 
+import Icons from './Components/Icons';
 import About from './Components/About';
 import Projects from './Components/Projects';
-import Contact from './Components/Contact'; 
-import Experience from './Components/Experience'; 
-import Technologies from './Components/Technologies'; 
+import Contact from './Components/Contact';
+import Experience from './Components/Experience';
+import Technologies from './Components/Technologies';
 
 function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <Switch>
-        <Route exact path="/" component={Logo} />
-        <Route path="/about" component={About} />
-        <Route path="/technologies" component={Technologies} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/experience" component={Experience} />
-      </Switch>
-      <Icons />
-    </div>
-  );
+   return (
+      <div className="App">
+         <NavBar />
+         <Switch>
+            <Route exact path="/" component={Logo} />
+            <Route path="/about" component={About} />
+            <Route path="/technologies" component={Technologies} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/experience" component={Experience} />
+         </Switch>
+         <Icons />
+      </div>
+   );
 }
 
 export default App;
