3a1ffed9c2f2623c94b12a293f2693b1a2c40155
myk245 <myk245@nyu.edu>
2020-09-18 20:49:45 -0400

    redesign of project page

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -10,7 +10,12 @@ function Projects() {
          <h1 className="page-title">Projects</h1>
          <br></br>
             <h6 className="page-subtitle">Below is a selection of my projects. Please feel free to contact me with any questions or comments.</h6>
-         <Paper className="paper-section" elevation={3} variant="outlined">
+         <Paper
+            elevation={1}
+            style={{ padding: 40, margin: 40, backgroundColor: "#f4f4f4" }}
+            variant="outlined"
+            className="paper-section"
+         >
             <br></br>
             <br></br>
             <div id="grid-div">
