6cc122d43106f02987c9ffd8a403bfa27b02af7a
myk245 <myk245@nyu.edu>
2020-12-29 18:14:58 -0500

    add publication

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -26,6 +26,10 @@ function Blog() {
                <CardContent>
                   <p>Algorithms are an important part of technical interviews and the only way to get better at them is to keep practicing. How can we approach coming up with a solution to a classic algorithm question: taking an input string and returning a Caesar Cipher encrypted version of it?</p>
                   <br></br>
+                  <p>
+                     <em>Published on JavaScript in Plain English</em>
+                  </p>
+                  <br></br>
                   <Divider></Divider>
                   <br></br>
                   <a target="_blank"
