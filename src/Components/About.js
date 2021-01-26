2d23364fa82f2c3fdee7938848043362eccfb896
myk245 <myk245@nyu.edu>
2021-01-25 23:21:31 -0500

    update about me

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,10 +6,9 @@ function About() {
       <div>
          <Paper
             elevation={1}
-            style={{ padding: 40, margin: 40, backgroundColor: "#fff0f0" }}
+            style={{ padding: 40, margin: 60, backgroundColor: "#F7F7F8"}}
             variant="outlined"
             >
-            {/* blue: #f0f8ff */}
             <Grid
                container
                justify="center"
@@ -22,23 +21,23 @@ function About() {
                      id="main-image"
                   />
                </Grid>
-               <Grid item xs={10} zeroMinWidth>
+               <Grid item xs={11} zeroMinWidth>
                   <br></br>
                   <br></br>
                   <Typography id="about-section">
-                     Hi, my name's Cindy! I was born in Hong Kong and raised in Queens, NY. 
+                     Hi, I'm Cindy, a Software Engineer based in NYC.
                      <br></br>
                      <br></br>
-                     I am a highly motivated, collaborative, and detail-oriented Fullstack Software Engineer who is passionate about creating social change and making a difference through innovative, powerful technologies. I have experience in e-commerce, sales, and customer service and have a proven track record in driving sales and delivering results. I am driven by the opportunity to use my technical and people skills to make an impact on peoples' lives and to deliver the best user experience. I am looking to join a forward-thinking company where I can utilize my strong problem solving, communication, and critical thinking skills in order to help create thoughtful, intelligent solutions that make an impact on people's lives.
+                     I am a highly motivated, collaborative, and detail-oriented Fullstack Software Engineer who is passionate about creating social change and making a difference through innovative, powerful technologies. I have experience in e-commerce and customer service and have a proven track record in driving sales and delivering results. I am driven by the opportunity to use my technical and people skills to make an impact on peoples' lives and to deliver the best user experience. I am looking to join a forward-thinking company where I can utilize my strong problem solving, communication, and critical thinking skills in order to help create thoughtful, intelligent solutions that make an impact on people's lives.
                      <br></br>
                      <br></br>
-                     I became interested in the creative exploration of good design and the potential of Software Engineering to reach a global audience, 
+                     I became interested in the creative exploration of web design and the potential of Software Engineering to reach a global audience, 
                      so I started learning to code on my own. I love the challenge of building something from scratch and seeing it come to life on my laptop 
                      screen. I enrolled in Flatiron School in order to immerse myself in the world of Software Engineering, meet people who 
-                     are passionate about coding, collaborate on exciting projects, and learn from some brilliant developers. 
+                     are passionate about coding, collaborate on exciting projects, and learn from brilliant developers. 
                      <br></br>
                      <br></br>
-                     In my downtime, I enjoy reading, eating, interior design, talking to my plant babies, watching puppy and baby videos, 
+                     In my downtime, I enjoy reading, eating (and cooking), looking at interior design trends, talking to my plants,
                      traveling, and experiencing the diverse languages, cultures, and cuisines of the world. 
                      <br></br>
                      <br></br>
