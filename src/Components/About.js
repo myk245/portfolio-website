225e348f2e18e490957d7ebb2c4410c27895bfff
myk245 <myk245@nyu.edu>
2021-08-05 00:13:29 -0400

    rework about me

--- src/Components/About.js
+++ src/Components/About.js
@@ -25,23 +25,19 @@ function About() {
                   <br></br>
                   <br></br>
                   <Typography id="about-section">
-                     Hi, I'm Cindy. I am a Software Engineer based in NYC.
+                     Hi, my name is Cindy and my pronouns are She/Her.
                      <br></br>
                      <br></br>
-                     I am a highly motivated, growth-minded, and detail-oriented Fullstack Developer who is passionate about creating social change and making a difference through innovative, powerful technologies. I have experience in e-commerce and customer service and have a proven track record in driving sales and delivering results. I am driven by the opportunity to use my technical and people skills to make an impact on peoples' lives and to deliver the best user experience. I am looking to join a forward-thinking company where I can utilize my strong problem solving, communication, collaboration, and critical thinking skills in order to help create thoughtful, intelligent solutions that make an impact on people's lives.
+                     I'm a Full Stack Software Engineer based in NYC who loves being able to create innovative solutions that leave a positive impact on the world. With previous experience in e-commerce and customer service, I have a proven track record in driving sales and delivering results. I love being a Software Engineer because it gives me the unique opportunity to use my technical, critical thinking, and people skills to make an impact on the lives of people from all over the world.
                      <br></br>
                      <br></br>
-                     I became interested in the creative exploration of web design and the potential of Software Engineering to reach a global audience, 
-                     so I started learning to code on my own. I love the challenge of building something from scratch and seeing it come to life on my laptop 
-                     screen. I enrolled in Flatiron School in order to immerse myself in the world of Software Engineering, meet people who 
-                     are passionate about coding, collaborate on exciting projects, and learn from brilliant developers. 
+                     I am well-versed in JavaScript, React, Ruby, and Rails and am always eager to learn new things in order to keep growing as a Software Engineer. I place a special emphasis on delivering the best user experience, in turn improving customer retention and driving company growth. I am looking to join an innovative and dynamic team where I can utilize my strong problem-solving, communication, collaboration, and project-management skills in order to help create thoughtful, intelligent solutions that will help ensure the growth and continued success of the company.
                      <br></br>
                      <br></br>
-                     In my downtime, I enjoy reading, eating (and cooking), looking at interior design trends, talking to my plants,
-                     traveling, and experiencing the diverse languages, cultures, and cuisines of the world. 
+                     When I am not coding or debugging, I enjoy traveling, cuddling all the kitties and puppies, being a plant mom, browsing interior design pages, cooking (and eating!), watching true crime documentaries, reading, and experiencing the diverse languages, cultures, and cuisines of the world.
                      <br></br>
                      <br></br>
-                     <b><i>I'd love to hear from you, so please feel free to reach out!</i></b>
+                     <b><i>If you'd like to connect, please feel free to leave me a message using the "Get In Touch" option in the dropdown menu (top right corner). You can also connect with me on LinkedIn or reach me via email at myk245@nyu.edu. I'd love to hear from you.</i></b>
                   </Typography> 
                </Grid>
             </Grid>
